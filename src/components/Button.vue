2092dc1ee53295044e24f9440cd743019afec14a
Walison Theodoro <theodoro.walison@gmail.com>
2024-06-08 12:43:38 -0300

    feat: add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface Props {
+  rounded?: boolean
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <button
+    :class="
+      props.rounded
+        ? 'px-4 py-2 bg-orange-light text-white rounded-full transition duration-300 ease-in-out hover:bg-orange-300'
+        : 'px-4 py-2 bg-gradient-to-r from-gradient-orange-from to-gradient-orange-to text-white rounded-md transition duration-300 ease-in-out hover:bg-orange-300'
+    "
+  >
+    <slot />
+  </button>
+</template>
